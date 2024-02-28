398cf31210dcd42a2be54d4de0a7496bbdd26af8
ibmtalNtp <160744240+ibmtalNtp@users.noreply.github.com>
2024-02-28 16:13:49 +0300

    mineFarm
    uygulama oluşturuldu

---
+++ minefarm/minefarm/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minefarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Mine m11;
+        private Mine m22;
+        private List<Mine> mine;
+        
+        private void checkBomb(object sender,EventArgs e) {
+            //sender değişkeni gelen nesneyi belirtir
+
+            Mine m = sender as Mine;//tıklanan buton m'dir
+        //    MessageBox.Show("Satır : "+m.row);
+        //    MessageBox.Show("sütun : "+m.column);
+           
+            if (m.bomb == 1)//tıklanan mine butonunun bomb'u 1 mi?
+            {
+                //m.bomb'un değeri 1 ise burası çalışacak
+                m.Text = "X";//tıklanan butona X yaz
+            }
+            else {
+                //m.bomb'un değeri 1 değil ise burası çalışacak
+                m.Visible = false;//tıklanan butonu gizle
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            m11 = new Mine(1,1,0);
+            m22 = new Mine(2,2,1);
+            this.Controls.Add(m11);
+            this.Controls.Add(m22);
+            */
+            mine = new List<Mine>();
+            this.Width = 416;
+            this.Height = 440;
+            int i, j;
+            Random rnd = new Random();
+            for (i = 0; i < 100;i++ )
+            {
+                mine.Add(new Mine(i/10,i%10,0));
+                mine[i].bomb = rnd.Next(2);
+                mine[i].Click += checkBomb;
+
+                this.Controls.Add(mine[i]);
+            }
+        }
+    }
+}
---
+++ minefarm/minefarm/Mine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace minefarm
+{
+    class Mine: Button
+    {
+        public int row;
+        public int column;
+        public int bomb;
+        public Mine() {
+ 
+        }
+        public Mine(int r,int c,int b) {
+            this.row = r;
+            this.column = c;
+            this.bomb = b;
+            this.Width = 40;
+            this.Height = 40;
+            this.Left = c * 40;
+            this.Top = r * 40;
+        }
+
+    }
+}
