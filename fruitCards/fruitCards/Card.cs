09234b7a18ee075e39f99c00f56e60dff8a6f2c4
ibmtalNtp <160744240+ibmtalNtp@users.noreply.github.com>
2024-03-06 16:38:42 +0300

    FruitCards project

---
+++ fruitCards/fruitCards/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace fruitCards
+{
+    class Card : PictureBox
+    {
+        public string[] fruits = { "apple.png", "grape.png", "ananas.png", "banana.png", "nar.jfif", "orange.jfif", "peach.jfif", "plum.jfif", "sweetcorn.png", "watermelon.jfif" };
+        public int fruitNumber;
+        public int row;
+        public int column;
+        public Card() {
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Width = 100;
+            this.Height = 100;
+            this.Image = Image.FromFile(@"..\..\fruits\card.png");
+            this.fruitNumber = -1;
+        }
+        public void setFruit(int meyveno) {
+            this.fruitNumber = meyveno;
+            this.Image = Image.FromFile(@"..\..\fruits\"+fruits[meyveno]);
+        }
+
+
+
+    }
+}
---
+++ fruitCards/fruitCards/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fruitCards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Card card;
+        private List<Card> cards=new List<Card>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            PictureBox r1 = new PictureBox();
+            r1.Image = Image.FromFile(@"..\..\fruits\apple.png");
+            r1.SizeMode = PictureBoxSizeMode.StretchImage;
+            r1.Width = 100;
+            r1.Height = 100;
+            this.Controls.Add(r1);
+             */
+            //card = new Card();
+            //card.setFruit(4);
+            //this.Controls.Add(card);
+            int i, j;
+            Card card;
+            Random r = new Random();
+            for (i = 0; i <4;i++ )
+            {
+                for (j = 0; j <4;j++ )
+                {
+                    card = new Card();
+                    card.setFruit(r.Next(0,10));
+                    card.Left = j * card.Width;
+                    card.Top = i * card.Height;
+                    card.row = i;
+                    card.column = j;
+                    cards.Add(card);
+                    this.Controls.Add(card);
+                }
+            }
+
+
+        }
+    }
+}
